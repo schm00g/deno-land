15910dca2f6cdcd1e663e3861e083f8fd572ebf7
schm00g <hk8igureu@relay.firefox.com>
2022-04-30 16:11:59 +0100

    unix cat program

---
+++ cat.ts
@@ -0,0 +1,6 @@
+import { copy } from "https://deno.land/std@0.137.0/streams/conversion.ts";
+for (const filename of Deno.args) {
+  const file = await Deno.open(filename);
+  await copy(file, Deno.stdout);
+  file.close();
+}
